5fd6679ea20ce3c7dd40ecb1bd2ebfa406ab7a2d
Mai El-Zein <mai_ez@outlook.dk>
2024-09-16 14:30:28 +0200

    button

--- js/forms.js
+++ js/forms.js
@@ -3,8 +3,13 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
   const login = document.querySelector("#Login");
   login.addEventListener("click", info);
+  document.querySelector("#submit").addEventListener("click", submit);
 }
 
 function info() {
   alert("Du er nu logget ind tillykke.");
+}
+
+function submit() {
+  alert("Du har submittet");
 }
